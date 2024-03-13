e40140761e699c28039aefb00dab689c9d6b4a86
Lilian JAOUEN <lilian.jaouen@edu.devinci.fr>
2024-03-13 16:06:36 +0100

    ajout de la lecture d'un image

--- pbScientifique/pbScientifique/Program.cs
+++ pbScientifique/pbScientifique/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+byte[] myfile = File.ReadAllBytes("C://Users//lilia//Documents//PbScientifique//Pour TD1//Pour TD1//pokemon.bmp");
+Console.WriteLine("header");
+int size_headerinfo = myfile[14];
+for (int i = 0; i < myfile.Length; i++)
+{
+    Console.Write(myfile[i] + " ");
+    if (i == 13) { Console.WriteLine("\n\n");
+        Console.WriteLine("Header Info");
+    }
+    if (i == 14 + size_headerinfo) {  Console.WriteLine("\n\n"); }
+
+      
+}
